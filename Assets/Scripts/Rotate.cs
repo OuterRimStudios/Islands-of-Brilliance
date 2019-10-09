a3a9c00b71837b8eaf4cd2ac85a54490e31ccfc6
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-10-09 15:59:19 -0500

    added foliage and other stuff

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Rotate(transform.forward * speed * Time.deltaTime);
+    }
+}
