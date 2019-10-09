52a53adc53b63ebf149fa688f5be7edc039a5310
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-10-09 17:03:44 -0500

    cleaned up build, merged all scenes into 1

---
+++ Assets/Scripts/AnimateCurve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Components;
+
+public class AnimateCurve : MonoBehaviour
+{
+    public BGCcCursorObjectTranslate translate;
+    public float speed = .5f;
+
+    void Update()
+    {
+
+        translate.Cursor.DistanceRatio = Mathf.MoveTowards(translate.Cursor.DistanceRatio, 1, speed * Time.deltaTime);
+
+        if (translate.Cursor.DistanceRatio == 1)
+            translate.Cursor.DistanceRatio = 0;
+    }
+}
